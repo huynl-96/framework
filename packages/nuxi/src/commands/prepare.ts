46e9a43c92c5be4b4290d8390267c97dab82de3a
Levi (Nguyễn Lương Huy) <huy.nguyen.luong96@gmail.com>
2022-11-13 08:46:48 +0700

    feat(nuxi): watch mode

--- packages/nuxi/src/commands/prepare.ts
+++ packages/nuxi/src/commands/prepare.ts
@@ -1,6 +1,9 @@
 import { buildNuxt } from '@nuxt/kit'
 import { relative, resolve } from 'pathe'
+import chokidar from 'chokidar'
 import consola from 'consola'
+import { debounce } from 'perfect-debounce'
+import { Nuxt } from '@nuxt/schema'
 import { clearDir } from '../utils/fs'
 import { loadKit } from '../utils/kit'
 import { writeTypes } from '../utils/prepare'
@@ -9,7 +12,7 @@ import { defineNuxtCommand } from './index'
 export default defineNuxtCommand({
   meta: {
     name: 'prepare',
-    usage: 'npx nuxi prepare',
+    usage: 'npx nuxi prepare [--watch, -w]',
     description: 'Prepare nuxt for development/build'
   },
   async invoke (args) {
@@ -17,11 +20,28 @@ export default defineNuxtCommand({
     const rootDir = resolve(args._[0] || '.')
 
     const { loadNuxt } = await loadKit(rootDir)
-    const nuxt = await loadNuxt({ rootDir, config: { _prepare: true } })
-    await clearDir(nuxt.options.buildDir)
+    let nuxt: Nuxt
 
-    await buildNuxt(nuxt)
-    await writeTypes(nuxt)
-    consola.success('Types generated in', relative(process.cwd(), nuxt.options.buildDir))
+    const load = async () => {
+      if (nuxt) { await nuxt.close() }
+
+      nuxt = await loadNuxt({ rootDir, config: { _prepare: true } })
+      await clearDir(nuxt.options.buildDir)
+      await writeTypes(nuxt)
+      // await buildNuxt(nuxt)
+      consola.success('Types generated in', relative(process.cwd(), nuxt.options.buildDir))
+    }
+
+    if (args.watch || args.w) {
+      const dLoad = debounce(load)
+      const watcher = chokidar.watch([rootDir], { ignoreInitial: true, persistent: true })
+      watcher.on('all', (event, _file) => {
+        if (!nuxt) { return }
+        dLoad()
+      })
+    }
+
+    await load()
+    return 'wait' as const
   }
 })
